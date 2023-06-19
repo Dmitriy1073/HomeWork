ec0dc3770fb24ac6819289c3cef814070a3b4d2d
Dmitriy Safronov <dims1073@gmail.com>
2023-06-19 16:22:56 +0800

    Summ of two integer numbers

--- Example_3_Summ/Program.cs
+++ Example_3_Summ/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-int numberA = 3;
+int numberA = 31;
 int numberB = 5;
 int summ = numberA+numberB;
 Console.WriteLine(summ);

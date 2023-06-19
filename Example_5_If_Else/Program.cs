7c8e2777ee7032e97b79ef78a4280fa8bcd0ca12
Dmitriy Safronov <dims1073@gmail.com>
2023-06-19 18:43:23 +0800

    доработан пятый пример

---
+++ Example_5_If_Else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string name=Console.ReadLine();
+if (name.ToLower() == "митяй")
+  {
+    Console.Write("Хренасе! Да это же ");
+    Console.Write(name);
+    Console.WriteLine("!!!!");
+  }
+else
+{
+  Console.Write("Добрый день "); 
+ Console.WriteLine(name);  
+}

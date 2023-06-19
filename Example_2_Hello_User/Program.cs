74c6d25d83ec93fd76783927ed219dbea5793662
Dmitriy Safronov <dims1073@gmail.com>
2023-06-19 15:38:18 +0800

    Приветствие юзера

--- Example_2_Hello_User/Program.cs
+++ Example_2_Hello_User/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Введите ваше имя");
+﻿Console.WriteLine(" Inpur your name");
 string username = Console.ReadLine();
-Console.Write("Привет!");
+Console.Write("Hello! ");
 Console.Write(username);
+Console.WriteLine("!");

193df8537bb599674cd38e83daafc5ba0325861a
Dmitriy Safronov <dims1073@gmail.com>
2023-06-21 10:19:01 +0800

    Пример 1 квадрат числа

---
+++ Semi1/Example_1_S/Program.cs
@@ -0,0 +1,17 @@
+﻿//Написать программу принимающую число и выдающее его квадрат
+Console.WriteLine("Введите целое число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num * num;
+Console.Write("Квадрат введенного числа - ");
+Console.WriteLine(result);
+
+if (num > 0)
+{
+    Console.WriteLine("Было введено положительное число");
+}
+else if (num == 0)
+    Console.WriteLine("Был введен 0");
+else 
+    Console.WriteLine("Было введено отрицательное число");
+Console.WriteLine("The END");

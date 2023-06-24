ebd09b429f0eb33d085e5ff09ba7d1ebc99ace9c
Dmitriy Safronov <dims1073@gmail.com>
2023-06-24 21:44:59 +0800

    Задачи второго семинара

---
+++ Example_8_Array/Program.cs
@@ -0,0 +1,40 @@
+﻿// int Max(int arg1, int arg2,int arg3)
+// {
+// int result=arg1;
+// if(arg2>result) result=arg2;
+// if(arg3>result) result=arg3;
+// return result;
+// }
+int[] array = { 11, 21, 13, 41, 15, 61, 17, 81, 19 };
+int n = array.Length;
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int z=0;
+while (count<n)
+{
+    if (num==array[count])
+    {
+    Console.WriteLine("введенное число совпадает с " + (count) + " элементом массива");
+    z=1;
+    break;
+    }
+
+count = count + 1;
+//
+}
+if (z==0)
+{
+    Console.WriteLine("совпадений не найдено");
+}
+
+//int max1=Max(a1,b1,c1);
+// int max2=Max(a2,b2,c2);
+// int max3=Max(a3,b3,c3);
+// int max=Max(max1,max2, max3);
+
+// второй вариант
+// int max=Max(Max(a1,b1,c1),
+//          Max(a2,b2,c2),
+//             Max(a3,b3,c3));
+// Console.WriteLine(max);
---
+++ Example_9_Metod/Program.cs
@@ -0,0 +1,56 @@
+﻿// Функции заполнения и печати массивов
+void FillArray(int[] collection)
+{
+    int length= collection.Length;
+     int index=0; 
+     while (index< length)
+     {
+        collection[index]=new Random().Next(1,10);
+        index++;
+     }
+}
+
+void PrintArray(int[] col)
+{
+    int count =col.Length;
+    int position=0;
+    while (position<count) 
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count=collection.Length;
+    int index=0;
+    int position=-1;
+    while(index<count)
+    {
+        if(collection[index]==find)
+        {
+            position=index;
+            break;
+        }
+        index++;
+    }
+    return position; 
+}
+
+
+
+
+Console.WriteLine("Введите размерность массива");
+int n=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Введите искомое число");
+int found=Convert.ToInt32(Console.ReadLine());
+int[] array=new int[n];
+Console.WriteLine();
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos= IndexOf(array,found); 
+Console.WriteLine(pos); 

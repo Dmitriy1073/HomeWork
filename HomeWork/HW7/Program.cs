0dd8598a0abfaa108ca0ebc984cb70cd4ba9f4e0
Dmitriy Safronov <dims1073@gmail.com>
2023-08-23 19:40:52 +0800

    Домашнее заадние семинар 7

--- HomeWork/HW7/Program.cs
+++ HomeWork/HW7/Program.cs
@@ -1,13 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-// Урок 7. Двумерные массивы
-// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-// m = 3, n = 4.
-// 0,5 7 -2 -0,2
-// 1 -3,3 8 -9,9
-// 8 7,8 -7,1 9
+﻿
 
 
 
@@ -28,7 +19,7 @@ void FillArray2DManual(double[,] array)// функция заполнения 2D
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write($"Введите {i},{j} элемент массива");
+            Console.Write($"Введите {i},{j} элемент массива- ");
             array[i, j] = Convert.ToDouble(Console.ReadLine());
         }
     }
@@ -80,13 +71,20 @@ void FindMedianColuns(double[,] array)// Функция находит и выв
         {
             Sum = Sum + array[i, j];
         }
-        Console.WriteLine($" Сумма элементов {j} столбца= {Sum}");
-        Median[j] = Sum / array.GetLength(0);
+        //Console.WriteLine($" Сумма элементов {j} столбца= {Sum}");
+        Median[j] =Math.Round(Sum / array.GetLength(0),2);
         Console.WriteLine($" Среднее арифметическое {j} столбца= {Median[j]}");
 
     }
 }
-// тело программы для задачи №47
+
+// Урок 7. Двумерные массивы
+// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
 Console.Clear();
 Console.WriteLine("Введите число строк массива");
 int m = Convert.ToInt32(Console.ReadLine());
@@ -99,6 +97,7 @@ double[,] array2 = new double[m, n];
 //int rightBord = Convert.ToInt32(Console.ReadLine());
 //FillArray2D(array2, leftBord, rightBord);
 FillArray2DManual(array2);
+Console.Clear();
 Console.WriteLine("Исходный массив");
 PrintArrayForm2D(array2);
 
@@ -110,21 +109,22 @@ PrintArrayForm2D(array2);
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 // ввод и заполнение массива случайными числами см. задачу 47
-// закоментим пока Console.WriteLine("Введите искомый элемент массива");
-// int Num = Convert.ToInt32(Console.ReadLine());
-// FindNum2D(array2,Num);
+Console.WriteLine("Введите искомый элемент массива");
+int Num = Convert.ToInt32(Console.ReadLine());
+FindNum2D(array2,Num);
 
 
 
 
-// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее 
+//арифметическое элементов в каждом столбце.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 // ввод и заполнение массива случайными значениями см. задачу 47
-// пока закоментим FindMedianColuns(array2);
+FindMedianColuns(array2);
 
 
 
